e25fa9318d91a7489c6ab6ed4d62769e8aa64b01
Jessé-Manp <ti2manptecnologia@gmail.com>
2024-05-23 10:10:05 -0300

    Finalizado introdução ao conceito

--- Introdução ao conceito/5-Tensor.py
+++ Introdução ao conceito/5-Tensor.py
@@ -9,6 +9,9 @@ tensorn = np.array([[23, 4],[32, 51]])
 tensor_numpy = tf.constant(tensorn)
 #Defino uma variavel tensor
 tf_variavel = tf.Variable([[1., 2., 3.], [4., 5., 6.]])
+#Tensor com String
+tf_string = tf.constant("JeSse")
+tf_string2 = tf.constant(["Jessé", "python", "TensorFlow"])
 
 print(tensor)
 print(tensorn)
@@ -18,3 +21,23 @@ print(tf_variavel)
 #Manipulando variáveis
 tf_variavel[0, 2].assign(100)
 print(tf_variavel)
+
+print (tensor + 2)
+print (tensor * 2)
+print (tensor + tensor)
+print (tensor * tensor)
+print (tensor / tensor)
+
+print(tensor > tensorn)
+
+print(np.square(tensor))
+print(np.sqrt(tensor))
+print(np.dot(tensor, tensor))
+print(tf_string)
+
+print(tf.strings.length(tf_string))
+
+print(tf.strings.unicode_decode(tf_string, "UTF8"))
+
+for i in tf_string2:
+    print(i)
---
+++ Introdução ao conceito/6-Sessões.py
@@ -0,0 +1,15 @@
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+
+num1 = tf.constant(8)
+num2 = tf.constant(15)
+
+soma_num = num1 + num2
+
+print(soma_num)
+print(type(soma_num))
+
+with tf.Session() as sess:
+    soma = sess.run(soma_num)
+
+print(soma)
