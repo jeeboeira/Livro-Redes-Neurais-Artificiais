54c5b4b032abe9395e38893974253df2c3a726fa
Jessé-Manp <ti2manptecnologia@gmail.com>
2024-06-17 11:00:44 -0300

    Finalizado Reconhecimento de Caracteres :tada:

--- Redes_Neurais_convolucionais/1-Reconhecimento-de-Caracteres.py
+++ Redes_Neurais_convolucionais/1-Reconhecimento-de-Caracteres.py
@@ -5,14 +5,19 @@ from sklearn.model_selection import train_test_split
 from sklearn import svm
 from sklearn import metrics
 import matplotlib.pyplot as plt
+#Carrega funções de manipulação de imagem
 import matplotlib.image as mimg
+# Função para redimensionar imagens
+from skimage.transform import resize
+# Realiza a regressão logística
+from sklearn.linear_model import LogisticRegression
 
 base = datasets.load_digits()
 entradas = base.data
 saidas = base.target
 
-print(entradas[0])
-print(base.images[0])
+#print(entradas[0])
+#print(base.images[0])
 
 #Printa uma imagem
 plt.figure(figsize = (2, 2))
@@ -32,18 +37,37 @@ previsor = classificador.predict(eteste)
 
 margem_acerto = metrics.accuracy_score(steste, previsor)
 
-imagem = mimg.imread(r'C:\Users\ti2\TestVsCode\Livro-Redes-Neurais-Artificiais-1\Redes_Neurais_convolucionais\Arquivos_Convolucionais\1Um\num2.png')
-print(imagem)
+# Carrega uma imagem como array nump
+imagem = mimg.imread(r'C:\Users\ti2\TestVsCode\Livro-Redes-Neurais-Artificiais-1\Redes_Neurais_convolucionais\Arquivos_Convolucionais\1Um\num2.1.png')
 
-def rgb2gray(rgb):
-    # Recebe o produto escalar de todas as linhas e das 3 colunas vistas anteriormente
-        #sobre os valores 0.299,0.587,0.114, uma convenção de conversão rgb
-    img_array = np.dot(rgb[...,:3],[0.299,0.587,0.114])
-    # Trsanforma o resultado para inteiro com a função .astype
-    img_array2 = (16 - (img_array * 16)).astype(int)
-    # Converte para uma matriz indexada de 64 valores, em um intervalo de 0 a 16
-        #escala de cinza 
-    img_array3 = img_array2.flatten()
-    return img_array3
+#Função para tratar a imagem
+def rgb2gray(imagem):
+    # Redimensiona a imagem para 8x8 pixels (outra forma de reduzir para 64 posições)
+    imagem = resize(imagem, (8, 8), anti_aliasing = True)
+    # Seleciona os canais RGB da imagem
+    rgb_channels = imagem[...,:3]
+     # Aplica os pesos de conversão para obter a escala de cinza
+    imagem = np.dot(rgb_channels, [0.299, 0.587, 0.114])
+    # Ajusta o intervalo da escala de cinza para 0-16
+    imagem = (16 - (imagem * 16)).astype(int)
+    # Aplica a função flatten que converte uma array numpy multidimensional, e um array unidimensional
+    return imagem.flatten()
 
-rgb2gray(imagem)
+#Utilizo Suport Vector Machine para fazer a previsão da imagem que eu fornecer
+identificador = svm.SVC()
+identificador.fit(entradas, saidas)
+previsor_id = identificador.predict([rgb2gray(imagem)])
+print(previsor_id)
+
+#Faz uma regressão logistica
+logr = LogisticRegression()
+logr.fit(etreino, streino)
+previsor_logr = logr.predict(eteste)
+acerto_logr = metrics.accuracy_score(steste, previsor_logr)
+print(acerto_logr)
+
+#Regressor
+regressor = LogisticRegression()
+regressor.fit(entradas, saidas)
+previsor_regl = regressor.predict([rgb2gray(imagem)])
+print(previsor_regl)
